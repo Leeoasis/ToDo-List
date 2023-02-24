ad9fc145e64274c45ec55926148cb3091cd14bab
Leslie <Kooliesk@gmail.com>
2023-02-24 15:22:08 +0200

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   optimization: {
     runtimeChunk: 'single',
