839dd252dc96de71b40605c8d3ee4daa3e11ee28
Lengushuru Charles <67843085+lengushuru@users.noreply.github.com>
2023-03-02 12:53:21 +0300

    Add `remove` function tests.

--- modules/modules.test.js
+++ modules/modules.test.js
@@ -29,3 +29,49 @@ describe('add task', () => {
     expect(result).toEqual(expectedTask);
   });
 });
+jest.mock('./storage', () => ({
+  Storage: jest.fn(),
+  getStorage: jest.fn(),
+}));
+
+describe('remove task', () => {
+  let taskIndex;
+  let storedTasks;
+
+  beforeEach(() => {
+    // Set up the test data
+    taskIndex = 1;
+    storedTasks = [
+      { index: 1, completed: false, description: 'Task 1' },
+      { index: 2, completed: false, description: 'Task 2' },
+      { index: 3, completed: false, description: 'Task 3' },
+    ];
+    // Mock the getStorage function to return the test data
+    require('./storage').getStorage.mockReturnValue(storedTasks);
+  });
+
+  afterEach(() => {
+    // Reset the mock functions
+    jest.resetAllMocks();
+  });
+
+  it('should remove the task from the list', () => {
+    // Call the function to be tested
+    trashTask(taskIndex);
+    // Check that the task was removed from the list
+    expect(require('./storage').Storage).toHaveBeenCalledWith([
+      { index: 1, completed: false, description: 'Task 1' },
+      { index: 2, completed: false, description: 'Task 3' },
+    ]);
+  });
+
+  it('should update the task indices', () => {
+    // Call the function to be tested
+    trashTask(taskIndex);
+    // Check that the remaining tasks have their indices updated
+    expect(require('./storage').Storage).toHaveBeenCalledWith([
+      { index: 1, completed: false, description: 'Task 1' },
+      { index: 2, completed: false, description: 'Task 3' },
+    ]);
+  });
+});
